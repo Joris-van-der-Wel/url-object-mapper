61538937d007acb68aa616602691240664b491fc
Joris van der Wel <joris@jorisvanderwel.com>
2015-08-05 16:21:18 +0200

    Support for custom objects (custom constructor, matcher, etc)

--- test/Entry.js
+++ test/Entry.js
@@ -106,5 +106,73 @@ test('fromURL: redundant encoding should be normalized before matching', functio
         t.end();
 });
 
+class MyObject {
+        constructor(abc, def) {
+                this.myAbc = abc;
+                this.myDef = def;
+        }
+}
+
+const myObjectTemplate = {};
+myObjectTemplate.create = function(values) {
+        return new MyObject(values.abc, values.def);
+};
+
+myObjectTemplate.isValid = function(object) {
+        return object instanceof MyObject;
+};
+
+myObjectTemplate.serializeValue = function(object) {
+        return {
+                abc : object.myAbc,
+                def : object.myDef
+        };
+};
+
+test('fromURL: create custom object', function(t) {
+        const entry = new Entry('/foo/{1.abc}/{1.def}', ['foo', myObjectTemplate]);
+        const result = entry.fromURL('/foo/qwerty/dvorak');
+        t.deepEqual(result, ['foo', new MyObject('qwerty', 'dvorak')]);
+        t.ok(result && result[1] instanceof MyObject);
+        t.end();
+});
+
+test('fromURL: create custom object (deep)', function(t) {
+        const entry = new Entry('/foo/{1.bar.abc}/{1.bar.def}', ['foo', {bar: myObjectTemplate}]);
+        const result = entry.fromURL('/foo/qwerty/dvorak');
+        t.deepEqual(result, ['foo', {bar: new MyObject('qwerty', 'dvorak')}]);
+        t.ok(result && result[1] && result[1].bar instanceof MyObject);
+        t.end();
+});
+
+
+test('fromURL: create custom object without setting any values for it', function(t) {
+        const entry = new Entry('/foo', ['foo', myObjectTemplate]);
+        const result = entry.fromURL('/foo');
+        t.deepEqual(result, ['foo', new MyObject(undefined, undefined)]);
+        t.ok(result && result[1] instanceof MyObject);
+        t.end();
+});
+
+test('toURL: only match valid custom objects', function(t) {
+        const entry = new Entry('/foo/{1.abc}/{1.def}', ['foo', myObjectTemplate]);
+        t.equal(entry.toURL(['foo', {abc: 1, def: 2}]), null,
+                'should fail the isValid() check of the custom object template');
+        t.end();
+});
+
+test('toURL: serialize custom objects', function(t) {
+        const entry = new Entry('/foo/{1.abc}/{1.def}', ['foo', myObjectTemplate]);
+        t.equal(entry.toURL(['foo', new MyObject('qwerty', 'dvorak')]), '/foo/qwerty/dvorak');
+        t.end();
+});
+
+test('toURL: serialize custom objects (deep)', function(t) {
+        const entry = new Entry('/foo/{1.bar.abc}/{1.bar.def}', ['foo', {bar: myObjectTemplate}]);
+        t.equal(entry.toURL(['foo', {bar: new MyObject('qwerty', 'dvorak')}]), '/foo/qwerty/dvorak');
+        t.end();
+});
+
+
 // todo query string
 
